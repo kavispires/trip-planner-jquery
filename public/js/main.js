30f41b239e04f0a273d63743a2e73a653772cf6c
Alan Campbell <alancampbell4444@gmail.com>
2017-02-01 07:05:50 -0800

    s

--- public/js/main.js
+++ public/js/main.js
@@ -60,174 +60,156 @@ $(function initializeMap (){
   drawMarker('restaurant', [40.705137, -74.013940]);
   drawMarker('activity', [40.716291, -73.995315]);
 
-  init();
-});
+  var currentDay = 1;
+  var dayPlans = [{}];
+// $('.remove').prop("disabled",true);
 
-// Populate DOM 
-function init() {
-  // Loop through hotels and populate #hotel-choices
-  hotels.forEach(function(hotel, index, array) {
-    var thehtml = '<option value="' + hotel.id + '">' + hotel.name + '</option>';
-    $('#hotel-choices').append(thehtml);
-  });
-  // Loop through restaurants and populate #restaurant-choices
-  restaurants.forEach(function(restaurant, index, array) {
-    var thehtml = '<option value="' + restaurant.id + '">' + restaurant.name + '</option>';
-    $('#restaurant-choices').append(thehtml);
-  });
-  // Loop through activtes and populate #activity-choices
-  activities.forEach(function(activity, index, array) {
-    var thehtml = '<option value="' + activity.id + '">' + activity.name + '</option>';
-    $('#activity-choices').append(thehtml);
-  });
-  // Add the first day to itinerary
-  user.itineraries.push(user.dayTemplate);
-}
+//populate select options
+hotels.forEach(function(hotel){
+  $('#hotel-choices').append( `<option value=${hotel.name}> ${hotel.name}</option>`)   
+})
+restaurants.forEach(function(restaurant){
+  $('#restaurant-choices').append( `<option value=${restaurant.name}> ${restaurant.name}</option>`)   
+})
+activities.forEach(function(activity){
+  $('#activity-choices').append( `<option value=${activity.name}>${activity.name}</option>`)   
+})
 
-var user = {
-  // Store user's current choice on the select menus
-  selectedHotel: 1,
-  selectedRestaurant: 1,
-  selectedActivity: 1,
+//small blue plus buttons functionality
+$('#hotel-add').click(function(){
+  if(dayPlans[currentDay-1]===undefined){
+    dayPlans.push({})
+  } 
+  dayPlans[currentDay-1].hotel_name = $('#hotel-choices').val()
+  $('#hot-name').text(dayPlans[currentDay-1].hotel_name)
+})
 
-  currentDay: 1,
-  
-  itineraries: [],
-
-  dayTemplate: {
-      hotel: '',
-      restaurants: [],
-      activities: []
+$('#restaurant-add').click(function(){
+  if(dayPlans[currentDay-1]===undefined){
+    dayPlans.push({})
   }
-};
+  if(dayPlans[currentDay-1].restaurant_name ===undefined) { 
+    dayPlans[currentDay-1].restaurant_name = [$('#restaurant-choices').val()]
+  }else {
+    if(!dayPlans[currentDay-1].restaurant_name.includes($('#restaurant-choices').val())) {
+      dayPlans[currentDay-1].restaurant_name.push($('#restaurant-choices').val() )
+    }
+  }
+  $('#my-restaurants').empty();
+  dayPlans[currentDay-1].restaurant_name.forEach(function(rest) {
+    $('#my-restaurants').append(`<li style='list-style-type:none'>${rest}</li>`)
+  })
 
-/* ===============
-   OPTIONS PANEL 
-   =============== */
+})
 
-// Watches for changes in #hotel-choices and updates the user selection
-$('#hotel-choices').on('change', function() {
-  user.selectedHotel = +$(this).val();
-});
+$('#activity-add').click(function(){
+  if(dayPlans[currentDay-1]===undefined){
+    dayPlans.push({})
+  } 
+  if(dayPlans[currentDay-1].activity_name ===undefined) { 
+    dayPlans[currentDay-1].activity_name = [$('#activity-choices').val()]
+  }else {
+    if(!dayPlans[currentDay-1].activity_name.includes($('#activity-choices').val())) {
+      dayPlans[currentDay-1].activity_name.push($('#activity-choices').val() )
+    }
+  }
+  $('#my-activities').empty();
+  dayPlans[currentDay-1].activity_name.forEach(function(act) {
+    $('#my-activities').append(`<li style='list-style-type:none'>${act}</li>`)
+  })
+})
 
-$('#hotel-add').on('click', function() {
-  // Update hotel name inside itineraries
-  user.itineraries[user.currentDay - 1].hotel = hotels[user.selectedHotel - 1];
-  // Update itinerary in the DOM
-  refreshItinerary();
-});
+// add day button
+$('#day-add').click(function(){
+  dayPlans.push({})
+  $('#hot-name').text('')
+  $('#my-restaurants').empty();
+  $('#my-activities').empty();
+  $('.current-day').removeClass('current-day')
+  $(`<button id="justday" value=${dayPlans.length} class="btn btn-circle day-btn day current-day">${dayPlans.length}</button>`).insertBefore(this)
+  currentDay = dayPlans.length
+  $('#day-title span').text(`Day ${currentDay}`);
 
-$('#restaurant-choices').on('change', function() {
-  user.selectedRestaurant = +$(this).val();
-});
+})
 
-$('#restaurant-add').on('click', function() {
-  // Update hotel name inside itineraries
-  user.itineraries[user.currentDay - 1].restaurants.push(restaurants[user.selectedRestaurant - 1]); 
-  // Update itinerary in the DOM
-  refreshItinerary();
-});
-
-$('#activity-choices').on('change', function() {
-  user.selectedActivity = +$(this).val();
-});
-
-$('#activity-add').on('click', function() {
-  // Update hotel name inside itineraries
-  user.itineraries[user.currentDay - 1].activities.push(activities[user.selectedActivity - 1]); 
-  // Update itinerary in the DOM
-  refreshItinerary();
-});
-
-/* ===============
-   ITINERARY 
-   =============== */
-
-// Clicking on a day button, toggles current-day class and updates currentDay 
+//change active class of button and redefine current day variable
 $('.day-buttons').on('click', '.day', function(){
   $('.day').removeClass('current-day');
   $(this).addClass('current-day');
-  user.currentDay = +$(this).text();
-  $('#day-title span').text('Day ' + user.currentDay);
-  refreshItinerary();
-});
+  currentDay = $(this).text()
+  $('#day-title span').text(`Day ${currentDay}`);
+  //populate html with current days' data
+  $('#hot-name').empty()
+  $('#my-restaurants').empty();
+  $('#my-activities').empty();
 
-// Adding a new day, pushes a template object to user.itineraries and creates a new button
-$('#day-add').on('click', function() {
-  user.itineraries.push(user.dayTemplate);
-  $("<button class='btn btn-circle day-btn day'>" + user.itineraries.length + "</button>").insertBefore($(this));
-});
+  $('#hot-name').text(dayPlans[currentDay-1].hotel_name)
 
-// Clicking on the main day X button, removes that day completely from the database
-// It also changes the currentDay to a previous one and refresh the day numbers
-$('#delete-day').on('click', function(){
-  $('.day-buttons button:nth-child(' + user.currentDay + ')').remove();
-  user.itineraries.splice(user.currentDay - 1, 1);
-
-  // If that was the only day available, create a blank one
-  if (user.itineraries.length < 1) {
-     user.itineraries.push(user.dayTemplate); 
+  if(dayPlans[currentDay-1].restaurant_name) {
+    dayPlans[currentDay-1].restaurant_name.forEach(function(rest) {
+      $('#my-restaurants').append(`<li style='list-style-type:none'>${rest}</li>`)
+    })
   }
-  // Updates currentDay
-  user.currentDay = 1;
-  // Select the first day button and turn it active, then update the number in DOM.
-  $('.day-buttons button:first-Child').addClass('current-day');
-  $('#day-title span').text('Day ' + user.currentDay);
-  // Re-enumerate the days left
-  var renumbering = 1;
-  $('.day-buttons').children().each(function() {
-    if ($(this).hasClass('day')) {
-      $(this).text(renumbering++);
+
+  if(dayPlans[currentDay-1].activity_name) {
+    dayPlans[currentDay-1].activity_name.forEach(function(act) {
+      $('#my-activities').append(`<li style='list-style-type:none'>${act}</li>`)
+    })
+  }
+
+}); //day-buttons end
+
+
+$('.remove').click(function(){
+  if($('.current-day').prev().length) {
+    dayPlans.splice(currentDay-1,1)
+    $('.current-day').prev().addClass('current-day')
+    $('.current-day').last().remove()
+  }else {
+    if( $('.current-day').next().hasClass('adder')) {
+      dayPlans = [{}]
+    }else {
+      dayPlans.splice(currentDay-1,1)
+      $('.current-day').next().addClass('current-day')
+      $('.current-day').first().remove()
+      $('.current-day').val(1)
     }
-  });
-});
-
-// Get currentDay and populate #itinerary according to that day's data
-function refreshItinerary() {
-
-  var currentDay = user.itineraries[user.currentDay - 1];
-
-  // Add hotel
-  var hotelItem = itemTemplate(currentDay.hotel.name);
-  $('#my-hotels').html(hotelItem);
-
-  // Add restaurants
-  $('#my-restaurants').html(''); // clears DOM element
-  currentDay.restaurants.forEach(function(restaurant, index) {
-    var restaurantItem = itemTemplate(currentDay.restaurants[index].name);
-    $('#my-restaurants').append(restaurantItem);
-  });
-
-  // Add activities
-  $('#my-activities').html(''); // clears DOM element
-  currentDay.activities.forEach(function(activity, index) {
-    var activityItem = itemTemplate(currentDay.activities[index].name);
-    $('#my-activities').append(activityItem);
-  });
-}
-
-// Returns an HTML template for ech itinerary item
-function itemTemplate(name) {
-  if (name) {
-    return  '<div class="itinerary-item">' +
-            '<span class="title">' + name +'</span>' +
-            '<button class="btn btn-xs btn-danger remove btn-circle">x</button>' +
-            '</div>';  
-  } else {
-    return '';
   }
-}
 
-// NOT WORKING: The refreshItinerary, on line 188 is not picking the right array element for some reason to updating the DOM.
+  $('.day').each(function () {
+
+    if($(this).val()>$('.current-day').val()) {
+      $(this).val( $(this).val()-1)
+    }
+    $(this).text($(this).val())
+  });
+
+  currentDay =  $('.current-day').val()
+  $('#day-title span').text(`Day ${currentDay}`);
+  if(dayPlans.length===0){
+    $('.remove').prop("disabled",true);
+  }
+  //repopulate fields
+  if(dayPlans.length===1&&Object.keys(dayPlans[0]).length===0){
+    $('#hot-name').text('')
+    $('#my-restaurants').empty();
+    $('#my-activities').empty();
+  }else {
+    $('#hot-name').text(dayPlans[currentDay-1].hotel_name)
+    $('#my-restaurants').empty();
+    dayPlans[currentDay-1].restaurant_name.forEach(function(rest) {
+      $('#my-restaurants').append(`<li style='list-style-type:none'>${rest}</li>`)
+    })
+    $('#my-activities').empty();
+      dayPlans[currentDay-1].activity_name.forEach(function(act) {
+    $('#my-activities').append(`<li style='list-style-type:none'>${act}</li>`)
+    })
+  }
+
+}) //remove end
 
 
-
-
-
-
-
-
-
+});
 
 
 
